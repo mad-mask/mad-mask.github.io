7b72da54ca5751196cd504e5b0d088de99f7e7bc
mask ser <appushijil72@gmail.com>
2023-06-18 13:17:55 +0530

    ok

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form')
+const contactMessage = document.getElementById('contact-message'),
+const sendEmail = (e) =>{
+	e.preventDefault()
+	
+	// serviceID - templateID - #form - publicKey
+	emailjs.sendForm('service_5a6zu0c', 'template_oj2bz0p','#contact--form','5O8iRNVFb6tp51ckr')
+	.then(() =>{ 
+		// Show sent message
+		contactMessage.textContent = 'Message sent successfully✅'
+		
+		// Remove message after five seconds
+		setTimeout(()=>{
+			contactMessage.textContent = ''
+			}, 5000)
+			
+			// Clear input fields
+			
+			contactForm.reset()
+		
+		}, () => {
+			// Show error message
+			contactMessage.textContent = 'Message not sent (service error)❌'
+			});
+	}
+	contactForm.addEventListener('submit', sendEmail)
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
